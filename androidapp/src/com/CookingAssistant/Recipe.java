3aeabce3b102cc2fb8ec5aa2b0270b724223f356
Calvin Liang <cliang4@berkeley.edu>
2011-04-07 17:23:42 -0700

    Added Recipe.java

---
+++ androidapp/src/com/CookingAssistant/Recipe.java
@@ -0,0 +1,11 @@
+package com.CookingAssistant;
+
+public class Recipe {
+	long id;
+	String cook_time;
+	String[] ingredients;
+	String serv_size;
+	String[] steps;
+	String photo_urls;
+	String desc;	
+}
