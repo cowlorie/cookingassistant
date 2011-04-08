f49937b44c42cc9e3251ba36e8b77d2687094386
Steven Wan <xa1cap0n3x@gmail.com>
2011-04-08 05:12:21 -0700

    added sync button/display

--- androidapp/src/com/CookingAssistant/HomeActivity.java
+++ androidapp/src/com/CookingAssistant/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.CookingAssistant;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,26 @@ public class HomeActivity extends Activity {
         final Button helpButton = (Button) findViewById(R.id.button2);
         helpButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+            	JsonParser jp = new JsonParser(new RecipeDbAdapter(HomeActivity.this));
+            	jp.parse();
+            	
+            	Dialog dialog = new Dialog(HomeActivity.this);
+            	dialog.setContentView(R.layout.sync_dialog);
+                dialog.setTitle("Synchronization Complete");
+                dialog.setCancelable(true);
+                dialog.setCanceledOnTouchOutside(true);
+                TextView text = (TextView) dialog.findViewById(R.id.textView1);
+                text.setText("New Recipes Added");
+                
+                Button button = (Button) dialog.findViewById(R.id.button1);
+                button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                    public void onClick(View v) {
+                        finish();
+                    }
+                });
+                
+                dialog.show();
             }
         });
     }
