298d1689a3b9965ebfcf2d2e6f7a828372ca4857
Calvin Liang <cliang4@berkeley.edu>
2011-04-08 13:29:18 -0700

    Added two more unittests.

--- cookingassistanttest/CookingAssistantTest/src/com/CookingAssistant/test/TestMain.java
+++ cookingassistanttest/CookingAssistantTest/src/com/CookingAssistant/test/TestMain.java
@@ -29,7 +29,7 @@ public class TestMain extends ActivityInstrumentationTestCase2<Mainpage> {
         super.tearDown();
     }
     
-    public void testRecipeIsSentToShoppingList() throws Exception {
+    public void testRecipeIsSentToShoppingList(){
         solo.clickOnMenuItem("Recipe");
         solo.clickOnText("Chocolate Wontons");
         solo.sendKey(Solo.MENU);
@@ -47,7 +47,43 @@ public class TestMain extends ActivityInstrumentationTestCase2<Mainpage> {
         solo.goBack();
         solo.clickOnText("Edit File Extensions");
         Assert.assertTrue(solo.searchText("application/robotium"));
-        
     }
+    
+    public void testRecipeIsFavorited(){
+        solo.clickOnMenuItem("Recipe");
+        solo.clickOnText("Chocolate Wontons");
+        solo.sendKey(Solo.MENU);
+        solo.clickOnButton("Add to Favorites");
+        solo.sendKey(Solo.CLOSED);
+        solo.sendKey(Solo.CLOSED);
+        solo.clickOnButton("Favorites");
+        
+        Assert.assertTrue(solo.searchText("Chocolate Wontons"));
+        
+        solo.clickOnText("txt");
+        solo.clearEditText(2);
+        solo.enterText(2, "robotium");
+        solo.clickOnButton("Save");
+        solo.goBack();
+        solo.clickOnText("Edit File Extensions");
+        Assert.assertTrue(solo.searchText("application/robotium"));   
+    }
+    
+    public void testRecipeSearched(){
+        solo.clickOnButton("Search");
+        solo.enterText(0, "Chocolate Wontons");
+        solo.clickOnButton("Search");
+        solo.clickOnText("Chocolate Wontons");
+        Assert.assertTrue(solo.searchText("Chocolate Wontons"));
+        
+        solo.clickOnText("txt");
+        solo.clearEditText(2);
+        solo.enterText(2, "robotium");
+        solo.clickOnButton("Save");
+        solo.goBack();
+        solo.clickOnText("Edit File Extensions");
+        Assert.assertTrue(solo.searchText("application/robotium"));   
+    }
+
 
 }
