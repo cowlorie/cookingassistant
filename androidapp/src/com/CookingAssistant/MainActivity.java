3522b55b7929b4fb6496274b2d093028dcb0f353
Steven Wan <xa1cap0n3x@gmail.com>
2011-04-20 02:02:34 -0700

    Changed look and feel of tab bar added recipes to tab bar

---
+++ androidapp/src/com/CookingAssistant/MainActivity.java
@@ -0,0 +1,5 @@
+package com.CookingAssistant;
+
+public class MainActivity {
+
+}
--- androidapp/src/com/CookingAssistant/Mainpage.java
+++ androidapp/src/com/CookingAssistant/Mainpage.java
@@ -1,58 +1,59 @@
 package com.CookingAssistant;
 
-import android.content.Intent;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.widget.TabHost;
 import android.app.TabActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 public class Mainpage extends TabActivity {
     /** Called when the activity is first created. */
 	
-	TabHost tabHost;
+	TabHost mTabHost;
 	
     @Override  
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        Resources res = getResources(); // Resource object to get Drawables
-        tabHost = getTabHost();  // The activity TabHost
-        TabHost.TabSpec spec;  // Resusable TabSpec for each tab
-        Intent intent;  // Reusable Intent for each tab
-
-        // Create an Intent to launch an Activity for the tab (to be reused)
-        intent = new Intent().setClass(this, HomeActivity.class);
-
-        // Initialize a TabSpec for each tab and add it to the TabHost
-        spec = tabHost.newTabSpec("Home").setIndicator("Home",
-                          res.getDrawable(R.drawable.home))
-                      .setContent(intent);
-        tabHost.addTab(spec);
-
-        // Do the same for the other tabs
-        intent = new Intent().setClass(this, SearchActivity.class);
-        spec = tabHost.newTabSpec("Search").setIndicator("Search",
-                          res.getDrawable(R.drawable.search))
-                      .setContent(intent);
-        tabHost.addTab(spec);
-
-        intent = new Intent().setClass(this, ShoppingListActivity.class);
-        spec = tabHost.newTabSpec("ShoppingList").setIndicator("ShoppingList",
-                          res.getDrawable(R.drawable.list))
-                      .setContent(intent);
-        tabHost.addTab(spec);
-        
-        intent = new Intent().setClass(this, FavoritesActivity.class);
-        spec = tabHost.newTabSpec("Favorites").setIndicator("Favorites",
-                          res.getDrawable(R.drawable.star))
-                      .setContent(intent);
-        tabHost.addTab(spec);
-
-        tabHost.setCurrentTab(0);
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.main);
+    	mTabHost = (TabHost) findViewById(android.R.id.tabhost);
+    	mTabHost.getTabWidget().setDividerDrawable(R.drawable.tab_divider);
+    	
+    	Intent intent;
+    	
+    	intent = new Intent().setClass(this, HomeActivity.class);
+    	setupTab(new TextView(this), "Home", intent, R.drawable.home);
+    	
+    	intent = new Intent().setClass(this, RecipeListActivity.class);
+    	setupTab(new TextView(this), "Recipes", intent, R.drawable.food);
+    	
+    	intent = new Intent().setClass(this, SearchActivity.class);
+    	setupTab(new TextView(this), "Search", intent, R.drawable.search);
+    	
+    	intent = new Intent().setClass(this, ShoppingListActivity.class);
+    	setupTab(new TextView(this), "Shop", intent, R.drawable.list);
+    	
+    	intent = new Intent().setClass(this, FavoritesActivity.class);
+    	setupTab(new TextView(this), "Favorites", intent, R.drawable.star);
     }
     
-    public void switchTab(int tab){
-        tabHost.setCurrentTab(tab);
+    private void setupTab(final View view, final String tag, Intent intent, int iconResource) {
+    	View tabview = createTabView(mTabHost.getContext(), tag, iconResource);
+        TabSpec setContent = mTabHost.newTabSpec(tag).setIndicator(tabview).setContent(intent);
+    	mTabHost.addTab(setContent);
     }
+
+    private static View createTabView(final Context context, final String text, int iconResource) {
+    	View view = LayoutInflater.from(context).inflate(R.layout.tabs_bg, null);
+    	TextView tv = (TextView) view.findViewById(R.id.tabsText);
+    	tv.setText(text);
+    	ImageView icon = (ImageView) view.findViewById(R.id.tabsIcon);
+		icon.setImageResource(iconResource);
+    	return view;
+    }
+
 }
